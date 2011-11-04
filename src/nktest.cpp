ce76c389d294a37661ffab79be15d9090e1ac591
Nick Stenning <nick@whiteink.com>
2011-11-04 20:34:48 +0000

    Simplify nktest

--- src/nktest.cpp
+++ src/nktest.cpp
@@ -8,8 +8,6 @@
 #include "ga/genome.h"
 #include "random.h"
 
-ga::Population population_from_stream(std::istream& is, unsigned int n);
-
 int main (int argc, char const *argv[])
 {
   unsigned int n, k;
@@ -30,28 +28,16 @@ int main (int argc, char const *argv[])
   util::random::init();
 
   nk::Landscape landscape(n, k);
-  ga::Population population = population_from_stream(std::cin, n);
+  ga::Population population;
+  ga::Genome g(n);
 
+  while(std::cin >> g) {
+    population.push_back(g);
+  }
 
-  for (unsigned int i = 0; i < population.size(); ++i) {
-    std::cout << landscape.evaluate(population[i]) << std::endl;
+  for (auto i = population.cbegin(); i < population.cend(); ++i) {
+    std::cout << landscape.evaluate(*i) << std::endl;
   }
 
   return 0;
 }
-
-ga::Population population_from_stream(std::istream& is, unsigned int n)
-{
-  ga::Population pop;
-
-  unsigned long ul;
-  std::string line;
-  while(std::getline(is, line)) {
-    ul = std::strtoul(line.c_str(), NULL, 0);
-    ga::Genome g(n, ul);
-    pop.push_back(g);
-  };
-
-  return pop;
-}
-
