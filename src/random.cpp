2df648d2d1e430611fcf3b231607abf9d8ae07bd
Nick Stenning <nick@whiteink.com>
2011-11-04 14:45:02 +0000

    Make rng instantiatable

--- src/random.cpp
+++ src/random.cpp
@@ -1,20 +1,63 @@
-#include <cmath>
 #include <cstdlib>
-#include <iostream>
 #include <random>
-#include <functional>
-#include <stdint.h>
+#include <cstdint>
 
 #include "random.h"
 
 static std::uniform_real_distribution<double> real_distribution;
 static std::uniform_int_distribution<uintmax_t> uint_distribution(0, UINTMAX_MAX);
 
-static std::mt19937_64 engine; // Mersenne twister MT19937
-static auto real_generator = std::bind(real_distribution, std::ref(engine));
-static auto uint_generator = std::bind(uint_distribution, std::ref(engine));
+static util::random::rng global_rng;
 
-uintmax_t util::random::init ()
+util::random::rng::rng ()
+: _engine()
+, _real_generator(std::bind(real_distribution, std::ref(_engine)))
+, _uint_generator(std::bind(uint_distribution, std::ref(_engine)))
+{
+  std::random_device dvc;
+  uintmax_t seed = dvc();
+
+  this->seed(seed);
+}
+
+util::random::rng::rng (uintmax_t seed)
+: _engine()
+, _real_generator(std::bind(real_distribution, std::ref(_engine)))
+, _uint_generator(std::bind(uint_distribution, std::ref(_engine)))
+{
+  this->seed(seed);
+}
+
+uintmax_t util::random::rng::seed (uintmax_t seed)
+{
+  this->_engine.seed(seed);
+  return seed;
+}
+
+double util::random::rng::random ()
+{
+  return this->_real_generator();
+}
+
+double util::random::rng::uniform (double a, double b)
+{
+  std::uniform_real_distribution<double> distribution(a, b);
+  return distribution(this->_engine);
+}
+
+uintmax_t util::random::rng::rand_uint (uintmax_t max)
+{
+  if (max == UINTMAX_MAX) {
+    return this->_uint_generator();
+  } else {
+    std::uniform_int_distribution<uintmax_t> distribution(0, max);
+    return distribution(this->_engine);
+  }
+}
+
+// Static global RNG
+
+void util::random::init ()
 {
   uintmax_t seed;
   char const* envSeed;
@@ -23,43 +66,26 @@ uintmax_t util::random::init ()
 
   if (envSeed != NULL) {
     seed = strtoul(envSeed, NULL, 0);
-  } else {
-    seed = std::time(0);
+    global_rng.seed(seed);
   }
-
-  std::cerr << "RNG_SEED=" << seed << "\n";
-
-  return util::random::seed(seed);
-}
-
-uintmax_t util::random::seed (uintmax_t seed)
-{
-  engine.seed(seed);
-  return seed;
 }
 
 double util::random::random ()
 {
-  return real_generator();
+  return global_rng.random();
 }
 
 double util::random::uniform (double a, double b)
 {
-  std::uniform_real_distribution<double> distribution(a, b);
-  return distribution(engine);
-}
-
-bool util::random::choose (double prob)
-{
-  return (util::random::random() < prob);
+  return global_rng.uniform(a, b);
 }
 
 uintmax_t util::random::rand_uint (uintmax_t max)
 {
-  if (max == UINTMAX_MAX) {
-    return uint_generator();
-  } else {
-    std::uniform_int_distribution<uintmax_t> distribution(0, max);
-    return distribution(engine);
-  }
+  return global_rng.rand_uint(max);
+}
+
+std::mt19937_64& util::random::get_engine ()
+{
+  return global_rng._engine;
 }
--- src/random.h
+++ src/random.h
@@ -1,6 +1,8 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+#include <random>
+#include <functional>
 #include <stdint.h>
 
 namespace util
@@ -11,16 +13,35 @@ namespace util
 **/
 namespace random
 {
+  class rng
+  {
+  public:
+    rng();
+    rng(uintmax_t seed);
+    virtual ~rng() {};
 
-  uintmax_t init ();
-  uintmax_t seed (uintmax_t seed);
+    uintmax_t seed (uintmax_t seed);
+
+    double random ();
+    double uniform (double a, double b);
+
+    uintmax_t rand_uint (uintmax_t max = UINTMAX_MAX);
+
+    friend std::mt19937_64& get_engine ();
+
+  private:
+    std::mt19937_64 _engine;
+    std::function<double()> _real_generator;
+    std::function<uintmax_t()> _uint_generator;
+  };
+
+  void init ();
 
   double random ();
   double uniform (double a, double b);
-
   uintmax_t rand_uint (uintmax_t max = UINTMAX_MAX);
 
-  bool choose (double prob = 0.5);
+  std::mt19937_64& get_engine();
 }
 
 }
