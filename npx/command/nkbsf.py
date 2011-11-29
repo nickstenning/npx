32ecbf5cdc6e3eec164f24b28ac8468323d1c4db
Nick Stenning <nick@whiteink.com>
2011-11-29 15:10:09 +0000

    Refactor parallel execution code

--- npx/command/nkbsf.py
+++ npx/command/nkbsf.py
@@ -1,61 +1,11 @@
 import argparse
-import random
 import sys
-from collections import namedtuple
-import numpy as np
+
 from IPython.parallel import Client
-
-from npx import nk
-from npx import bitarray
-
-class Individual(object):
-    def __init__(self, obj=None, fitness=0.0):
-        self.obj = obj
-        self.fitness = fitness
-
-class Population(list):
-    def __init__(self, *args, **kwargs):
-        super(Population, self).__init__(*args, **kwargs)
-        self.best_so_far = -1.0
-
-    def update_best_so_far(self):
-        fitnesses = np.array(map(lambda x: x.fitness, self))
-        max_ = fitnesses.max()
-        if not self.best_so_far or max_ > self.best_so_far:
-            self.best_so_far = max_
-
-def make_populations(npops, popsize, n):
-    return [make_population(popsize, n) for _ in xrange(npops)]
-
-def make_population(popsize, n):
-    return Population([Individual(bitarray.random_bitarray(n), 0.0) for _ in xrange(popsize)])
-
-def ga_init(populations, landscape):
-    # Initialize fitnesses
-    for pop in populations:
-        for ind in pop:
-            ind.fitness = landscape.evaluate(ind.obj.bin)
-
-def ga_tick(tick, populations, landscape):
-    for pop in populations:
-        pop.update_best_so_far()
-
-        cand_idx = random.randrange(len(pop))
-        other_idx = random.randrange(len(pop))
-
-        cand = Individual()
-        cand.obj = bitarray.mutate_single(pop[cand_idx].obj)
-        cand.obj = bitarray.crossover_binary_mpoint(cand.obj, random.choice(pop).obj, 4)
-        # cand.obj = bitarray.crossover_binary_uniform(cand.obj, random.choice(pop).obj, 0.2)
-
-        cand.fitness = landscape.evaluate(cand.obj.bin)
-
-        if cand.fitness > pop[other_idx].fitness:
-            pop[other_idx] = cand
-
 
 def print_bsfs(bsfs):
     print ("{:10.4g}" * len(bsfs)).format(*bsfs)
+    sys.stdout.flush()
 
 def main():
     parser = argparse.ArgumentParser(description='Run multiple simultaneous GAs and print their best-so-far statistics')
@@ -68,36 +18,36 @@ def main():
 
     args = parser.parse_args()
 
-    landscape = nk.Landscape(args.n, args.k, 651196428)
-
     rc = Client(profile=args.profile)
-    dview = rc[:]
+
+    dv = rc[:]
+    dv.block = True
+
+    from npx import nk
+    from npx import parallel
+
+    dv.execute('from npx import parallel')
+
+    landscape = nk.Landscape(args.n, args.k, 651196428)
+    dv['landscape'] = landscape
+    dv.execute('parallel.landscape = landscape')
 
     print >>sys.stderr, "# NK ID = %s" % landscape.id
     print >>sys.stderr, "# Running on %s processes" % len(rc)
 
-    dview.block = True
-
-    dview['landscape'] = landscape
-
-    dview.scatter('populations', range(args.npops))
-
-    dview['make_populations'] = make_populations
-    dview['ga_init'] = ga_init
-    dview['ga_tick'] = ga_tick
-
-    dview.execute('populations = make_populations(len(populations), %d, %d)' % (args.popsize, args.n))
-    dview.execute('ga_init(populations, landscape)')
-
-    dview['tick'] = tick = 0
+    tick = 0
+    dv.apply(parallel.make_populations, args.npops, args.popsize, args.n)
+    dv.apply(parallel.ga_init)
 
     while tick < args.popsize * args.ngens:
-        dview.execute('ga_tick(tick, populations, landscape)')
+        dv.apply(parallel.ga_tick)
 
         if tick % args.popsize == 0:
-            dview.execute('bsfs = [p.best_so_far for p in populations]')
-            bsfs = dview.gather('bsfs')
+            dv.execute('bsfs = [p.best_so_far for p in parallel.populations]')
+            bsfs = dv.gather('bsfs')
             print_bsfs(bsfs)
 
         tick += 1
-        dview.execute('tick += 1')
+
+if __name__ == '__main__':
+    main()
---
+++ npx/parallel.py
@@ -0,0 +1,52 @@
+import random
+
+from npx import nk
+from npx import bitarray
+
+landscape = None
+populations = None
+
+class Individual(object):
+    def __init__(self, obj=None, fitness=0.0):
+        self.obj = obj
+        self.fitness = fitness
+
+class Population(list):
+    def __init__(self, *args, **kwargs):
+        super(Population, self).__init__(*args, **kwargs)
+        self.best_so_far = -1.0
+
+    def update_best_so_far(self):
+        fitnesses = map(lambda x: x.fitness, self)
+        max_ = max(fitnesses)
+        if not self.best_so_far or max_ > self.best_so_far:
+            self.best_so_far = max_
+
+def make_population(popsize, n):
+    return Population([Individual(bitarray.random_bitarray(n), 0.0) for _ in xrange(popsize)])
+
+def make_populations(npops, popsize, n):
+    global populations
+    populations = [make_population(popsize, n) for _ in xrange(npops)]
+
+def ga_init():
+    # Initialize fitnesses
+    for pop in populations:
+        for ind in pop:
+            ind.fitness = landscape.evaluate(ind.obj.bin)
+
+def ga_tick():
+    for pop in populations:
+        pop.update_best_so_far()
+
+        cand_idx = random.randrange(len(pop))
+        other_idx = random.randrange(len(pop))
+
+        cand = Individual()
+        cand.fitness = landscape.evaluate(pop[cand_idx].obj.bin)
+
+        if cand.fitness > pop[other_idx].fitness:
+            cand.obj = bitarray.mutate_single(pop[cand_idx].obj)
+            if random.random() < 0.6:
+                cand.obj = bitarray.crossover_binary_uniform(cand.obj, random.choice(pop).obj, 0.2)
+            pop[other_idx] = cand
